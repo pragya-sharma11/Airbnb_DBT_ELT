60e0654505f497b645c9d3b6848339e0676c6075
pragya-sharma11 <pragya8528@gmail.com>
2026-07-18 00:56:18 +0530

    create macros

--- airbnb_dbt_elt/analyses/for_loop.sql
+++ airbnb_dbt_elt/analyses/for_loop.sql
@@ -2,6 +2,11 @@
 
 select 
 {% for col in cols %}
-    {{col}} {% if not loop.last %} , {% endif %}
+    {% if col == 'NIGHTS_BOOKED' %}
+        case when {{col}}>2 then {{multiply(col,2.43423,2)}} else {{col}} end as "nights_penalty" {% if not loop.last %} , {% endif %}
+    {%else%}
+        {{col}} {% if not loop.last %} , {% endif %}
+    {%endif%}
+
 {% endfor %}
 from {{ref('raw_bookings')}}
